3a78bdae6476d6316823d8a247bf862c3dc26d11
K-Holmes <Kayla.Holmes@cdpipelinedevshop.com>
2022-07-25 14:33:55 +0000

    first commit

---
+++ checkout/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Checkout from './Components/Checkout';
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout></Checkout>
+    </div>
+  );
+}
+
+export default App;
